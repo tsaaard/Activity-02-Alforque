afdb3a2990fb660d4f2cc2b7b231554b6ec3ca70
Richard Alforque <alforquerichard0611@gmail.com>
2022-04-02 00:26:26 +0800

    dual type added

--- mymod.py
+++ mymod.py
@@ -1,20 +1,22 @@
 import sys
+from time import sleep
 from os import system
 
 
 def chktypes(tayp):
     Types = ("Normal", "Fire", "Fighting", "Water", "Flying", "Grass", "Poison", "Electric", "Ground", "Psychic",
-             "Rock", "Ice", "Bug", "Dragon", "Ghost", "Dark", "Steel", "Fairy", "???")
+             "Rock", "Ice", "Bug", "Dragon", "Ghost", "Dark", "Steel")
 
     tflag = 0
-    for i in range(19):
+    for i in range(17):
         if tayp == Types[i]:
             tflag = 1
 
     if tflag == 0:
         print("Please select only on the list, restart program")
+        sleep(3)
         sys.exit()
-
+        
 def chkweather(weder):
     Weathers = ("Normal", "Sunny", "Rain", "Sandstorm", "Hail", "Other")
 
@@ -25,7 +27,58 @@ def chkweather(weder):
 
     if wflag == 0:
         print("Please select only on the list, restart program")
+        sleep(3)
         sys.exit()
+
+def dflagz(n1, n2):
+    val = 0
+    if n1 == 0 or n2 == 0:
+        val = 0
+    else:
+        if n1 == 0.5:
+            if n2 == 0.5:
+                val = 0.25
+            elif n2 == 1:
+                val = 0.5
+            elif n2 == 2:
+                val = 1
+        elif n2 == 0.5:
+            if n1 == 0.5:
+                val = 0.25
+            elif n1 == 1:
+                val = 0.5
+            elif n1 == 2:
+                val = 1
+        elif n1 == 1:
+            if n2 == 0.5:
+                val = 0.5
+            elif n2 == 1:
+                val = 1
+            elif n2 == 2:
+                val == 2
+        elif n2 == 1:
+            if n1 == 0.5:
+                val = 0.5
+            elif n1 == 1:
+                val = 1
+            elif n1 == 2:
+                val == 2
+        elif n1 == 2:
+            if n2 == 0.5:
+                val = 1
+            elif n2 == 1:
+                val = 2
+            elif n2 == 2:
+                val = 4
+        elif n2 == 2:
+            if n1 == 0.5:
+                val = 1
+            elif n1 == 1:
+                val = 2
+            elif n1 == 2:
+                val = 4   
+    return val
+
 
 def dmgtbst(dmg):
     system("cls")
@@ -37,25 +90,13 @@ def dmgtbst(dmg):
     print("Modifier:", dmg[4])
 
 def modtbst(mod):
-    if mod[2] == 2:
-        print("Data for Modifier")
-        print("Target:          ", mod[0])
-        print("Weather:         ", mod[1])
-        print("Generation:      ", mod[2])
-        print("Badge:           ", mod[3])
-        print("Critical:        ", mod[4])
-        print("Random:          ", mod[5])
-        print("STAB:            ", mod[6])
-        print("Effectiveness:   ", mod[7])
-        print("Burn:            ", mod[8])
-        print("other:           ", mod[9])
-    else:
-        print("Data for Modifier")
-        print("Target:          ", mod[0])
-        print("Weather:         ", mod[1])
-        print("Critical:        ", mod[4])
-        print("Random:          ", mod[5])
-        print("STAB:            ", mod[6])
-        print("Effectiveness:   ", mod[7])
-        print("Burn:            ", mod[8])
-        print("other:           ", mod[9])
+    print("Data for Modifier")
+    print("Target:          ", mod[0])
+    print("Weather:         ", mod[1])
+    print("Critical:        ", mod[2])
+    print("Random:          ", mod[3])
+    print("STAB:            ", mod[4])
+    print("Effectiveness:   ", mod[5])
+    print("Burn:            ", mod[6])
+    print("other:           ", mod[7])
+    sleep(5)
